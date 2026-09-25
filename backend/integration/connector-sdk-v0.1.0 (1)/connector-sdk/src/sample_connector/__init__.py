13ddde8ce3289450b2e71b3e2f6b0b12d7b3051f
Harshal sahare <harshsahare006@gmail.com>
2026-09-25 19:15:08 +0530

    Delete backend/integration/connector-sdk-v0.1.0 (1) directory

--- backend/integration/connector-sdk-v0.1.0 (1)/connector-sdk/src/sample_connector/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reference connector implementation (Section 27)."""
-
-from .client import SampleSourceClient
-from .connector import SampleConnector
-
-__all__ = ["SampleConnector", "SampleSourceClient"]
